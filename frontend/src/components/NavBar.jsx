9084cf27676a5c61a1c4b828f4612d70d821c2a8
Pavan Uthsara <uthsarapavan@gmail.com>
2025-03-21 12:00:16 +0530

    initialized tailwind success

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,14 +1,27 @@
+import {Link} from 'react-router-dom';
 
 function NavBar() {
     return (
         <>
-            <p>Navbar</p>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-                <li>Blog</li>
-            </ul>
+            <nav className="bg-green-500 p-4 shadow-lg">
+                <div className="max-w-7xl mx-auto flex justify-between items-center">
+                    <p className="text-white text-xl font-bold">Freshly</p>
+                    <ul className="flex space-x-6">
+                        <Link to="/">
+                        <li><a href="#" className="text-gray-300 hover:text-white transition duration-300">Home</a></li>
+                        </Link>
+
+                        <Link to="/create">
+                        <li><a href="#" className="text-gray-300 hover:text-white transition duration-300">Create</a></li>
+                        </Link>
+
+                        <li><a href="#" className="text-gray-300 hover:text-white transition duration-300"> Contact</a></li>
+
+                        <li><a href="#" className="text-gray-300 hover:text-white transition duration-300">Blog</a></li>
+                        
+                    </ul>
+                </div>
+            </nav>
         </>
     );
 }
